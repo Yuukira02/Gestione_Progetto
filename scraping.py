b4f6c847043b4f6369689f5fb91fb2078799e566
FatimaDahi10 <127510283+FatimaDahi10@users.noreply.github.com>
2024-01-25 20:07:12 +0100

    Add files via upload

--- scraping.py
+++ scraping.py
@@ -5,57 +5,68 @@ from newspaper.article import ArticleException
 from bs4 import BeautifulSoup
 
 def clean_filename(filename):
-    # Rimuovi i caratteri non consentiti nei nomi dei file
     return re.sub(r'[<>:"/\\|?*]', '', filename)
 
-# URL base
-base_url = 'https://www.repubblica.it/'
+def clean_html(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    story_text = soup.find(class_='ArticleText_article__text__xr3P1')
+    if story_text:
+        text = re.sub(r'&nbsp;', ' ', story_text.get_text(separator=' ', strip=True))
+        text = text.replace('\xa0', ' ')
+        return text
+    return None
 
-# Parole chiave associate a pubblicità
+#base_url_r = 'https://www.repubblica.it/'
+base_url = 'https://www.quotidiano.net/'
 ad_keywords = ['advertising', 'pubblicita', 'ads']
+categories = ['esteri', 'cronaca', 'economia', 'cultura', 'politica', 'scuola', 'salute', 'scienze']
 
-# Specifica le categorie desiderate
-categories = ['cronaca', 'economia', 'cultura']
+# Crea la cartella notizie se non esiste già
+notizie_folder = "./notizie"
+os.makedirs(notizie_folder, exist_ok=True)
 
 # Itera attraverso le categorie
-for category in categories:
+for category in categories :
     # Costruisci l'URL della categoria
     category_url = f'{base_url}{category}'
     category_news = build(category_url, memoize_articles=False)
-
-    # Crea la cartella per la categoria se non esiste già
-    category_folder = f"./{category}"
-    os.makedirs(category_folder, exist_ok=True)
 
     # Itera attraverso gli articoli della categoria
     for article in category_news.articles:
         try:
             # Verifica se l'URL contiene parole chiave associate a pubblicità
             if not any(keyword in article.url.lower() for keyword in ad_keywords):
-                # Verifica se l'articolo appartiene alla categoria specifica
-                if f'/{category}' in article.url.lower():
-                    # Costruisci l'oggetto Article
-                    article.build()
+                # Costruisci l'oggetto Article
+                article.build()
 
-                    # Estrai il testo dai tag <p> nella classe 'story__text'
-                    soup = BeautifulSoup(article.html, 'html.parser')
-                    text_content = ' '.join([p.get_text(strip=True) + ' ' for p in soup.select('.story__text p')])
+                # Verifica che il link dell'articolo esista nella cartella
+                #link_esistente = False
+                link_file_path = os.path.join(notizie_folder, f"{clean_filename(article.url)}.txt")
+                if os.path.exists(link_file_path):
+                    print(f"il file esiste già")
 
-                    # Altre informazioni dell'articolo
-                    title = article.title
-                    link = article.url
-                    publish_date = article.publish_date
+                else: # Se il link non esiste già, salva il link nella cartella
+                    # Estrai e pulisci l'HTML mantenendo solo il testo della classe 'story__text'
+                    text_content = clean_html(article.html)
+
+                    if text_content is not None:
+                        # Altre informazioni dell'articolo
+                        title = article.title
+                        link = article.url
+                        #publish_date = article.publish_date #REPUBBLICA
+                        publish_date = article.publish_date.strftime('%Y-%m-%d') if article.publish_date else None
+
 
                     # Pulisci il nome del file
-                    clean_title = clean_filename(title)
+                        clean_title = clean_filename(title)
 
-                    # Crea e scrivi il file .txt con le informazioni dell'articolo
-                    file_path = os.path.join(category_folder, f"{clean_title}.txt")
-                    with open(file_path, 'w', encoding='utf-8') as file:
-                        file.write(f"{title}\n")
-                        file.write(f"{link}\n")
-                        file.write(f"{publish_date}\n")
-                        file.write(f"{text_content}\n")
+                        link_file_path = os.path.join(notizie_folder, f"{clean_title}.txt")
+                        with open(link_file_path, 'w', encoding='utf-8') as file:
+                            file.write(f"{title}\n")
+                            file.write(f"{link}\n")
+                            file.write(f"{publish_date}\n")
+                            file.write(f"{text_content}\n")
+
 
         except ArticleException as e:
             print(f"Errore durante il recupero dell'articolo: {e}")

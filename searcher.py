02396fd44d2a9cacd26b7793a0c664d8a85b4192
FatimaDahi10 <127510283+FatimaDahi10@users.noreply.github.com>
2024-01-28 21:31:11 +0100

    Add files via upload

--- searcher.py
+++ searcher.py
@@ -1,102 +1,154 @@
+import re
+import nltk
+from whoosh.index import open_dir, exists_in
 from autocorrect import Speller
-from nltk.corpus import wordnet as wn, wordnet
-from whoosh.index import open_dir
 from whoosh.qparser import QueryParser, MultifieldParser
 from whoosh.qparser.dateparse import DateParserPlugin
-import tkinter as tk
+from nltk.corpus import wordnet as wn
+from transformers import pipeline
+from indexing import convert_predicted_sentiment, create_index
 
-from indexing import add_docs, custom_analyzer
+
+def disambiguate_terms(terms):
+    # controllo iniziale:
+    try:
+        # Prova a cercare il corpus di WordNet
+        nltk.data.find('corpora/wordnet')
+    except LookupError as e:
+        # Se non trova il corpus, scaricalo
+        nltk.download('wordnet')
+
+    terms = re.split(',|_|-|!| OR | NOT | AND |title:|content:|date:|sentiment:| ', terms)
+    related_words = set()
+    for t_i in terms:  # t_i is the target term
+        sel_sense = None
+        sel_score = 0.0
+
+        for s_ti in wn.synsets(t_i, lang='ita', pos=wn.NOUN):
+            score_i = 0.0
+            for t_j in terms:  # t_j term in t_i's context window
+                if t_i == t_j:
+                    continue
+                best_score = 0.0
+                for s_tj in wn.synsets(t_j, lang='ita', pos=wn.NOUN):
+                    temp_score = s_ti.wup_similarity(s_tj)
+                    if temp_score > best_score:
+                        best_score = temp_score
+                score_i = score_i + best_score
+            if score_i > sel_score:
+                sel_score = score_i
+                sel_sense = s_ti
+        if sel_sense is not None:
+            for sense in sel_sense.lemmas('ita'):
+                related_words.add(sense.name())
+    return related_words
 
 
-# TO-DO: in questo caso l'input è da terminale, più avanti sarà da GUI
-class SearchArticle:
-    def __init__(self, master):
-        self.master = master
-        master.title("Article Search")
+def get_related_words(word):
+    related_words = set()
 
-        self.query_label = tk.Label(master, text="Inserisci ciò che vuoi cercare:")
-        self.query_label.pack()
-
-        self.query_entry = tk.Entry(master)
-        self.query_entry.pack()
-
-        self.search_button = tk.Button(master, text="Cerca", command=self.search)
-        self.search_button.pack()
-
-        self.results_text = tk.Text(master, height=10, width=50)
-        self.results_text.pack()
-
-    # Funzione per ottenere le forme derivate e sinonimi di una parola usando nltk
-    def get_related_words(self, word):
-        related_words = set()
-
-        # Aggiungi la parola originale
-        related_words.add(word)
-
-        # Aggiungi le forme derivate
-        analyzer = custom_analyzer()
-        tokens = [token.text for token in analyzer(word)]
-        related_words.update(tokens)
-
+    tokens = word.split()
+    if len(tokens) > 1:
+        # Estendi con synset disambiguati dal contesto
+        related_words = disambiguate_terms(word)
+    else:
         # Aggiungi sinonimi
-        for syn in wordnet.synsets(word, pos=wn.NOUN, lang='ita'):
+        for syn in wn.synsets(word, pos=wn.NOUN, lang='ita'):
             for lemma in syn.lemmas('ita'):
                 related_words.add(lemma.name())
 
-        return list(related_words)
+    return list(related_words)
 
-    def search(self):
-        query=self.query_entry.get()
-        #query = input("Inserisci ciò che vuoi cercare (default: contenuto) \nSuggerito: 'Allarmante!': ")
 
+def Correction(word):
+    # Bypassa la correzione ortografica per la parola chiave "NOT"
+    if word.upper() == 'NOT':
+        return word
+    else:
         # Correttore ortografico
         spell = Speller(lang='it')
-        spell_word = spell(query)
+        return spell(word)
 
-        if query != spell_word:
-            print(f"Forse cercavi {spell_word}")
-            query = spell_word
 
-        #Ottieni sinonimi della parola inserita
-        synonyms = self.get_related_words(query)
+def execute_query(original_query, classifier, query_expansion, sentiment_analysis, correct_spelling, synonim):
+    """ALGORITMO CHE DATA UNA QUERY ESEGUE LE RISPOSTE"""
 
-        #Aggiugni la parola originale e i sinonimi alla query
-        full_query = f"{' OR '.join(synonyms)}"
+    total = 0
 
-        ix = open_dir("indexdir")
-        searcher = ix.searcher()
-        parser = MultifieldParser(["title", "content"], ix.schema)
-        #parser = QueryParser("content", ix.schema)
-        parser.add_plugin(DateParserPlugin())
-        query = parser.parse(full_query)
-        # results = searcher.search(query, limit=20) || results = searcher.search(query, limit=None)
-        results = searcher.search(query, limit=None)
+    if not exists_in("indexdir"):
+        print("Sto usando indexer da searcher")
+        create_index()
 
-        #for i in range(0, len(results)):
-         #   print(results[i])
-        # print(results[i] for i in list(range(0, 10)))
+    ix = open_dir("indexdir")
+    searcher = ix.searcher()
+    q = original_query
 
-        self.display_results(results)
+    if query_expansion:
+        if correct_spelling == True:
+            # Applica la correzione ortografica solo per le parole non booleane
+            corrected_query = " ".join(Correction(word) for word in q.split())
+            q = corrected_query
 
-    def display_results(self, results):
-        self.results_text.delete(1.0, tk.END)  # Pulisci il testo attuale
+        if synonim == True:
+            # Ottieni sinonimi della parola inserita
+            synonyms = get_related_words(q)
 
-        for result in results:
-            title = result["title"]
-            content = result["content"]
-            url = result["url"]
+            # Aggiugni la parola originale e i sinonimi alla query
+            if len(synonyms) != 0:
+                q = q + " AND (" + f"{' OR '.join(synonyms)}" + ")"
+                print(q)  # Debug
 
-        for result in results:
-            print(result)
+    # se stiamo eseguendo la V3 dell'engine, esegui
+    if sentiment_analysis:
+        if "sentiment" not in original_query:
+            # valuta la sentiment di una query per aumentare la capacità espressiva
+            prediction = classifier(original_query)
+            sentiment = convert_predicted_sentiment(prediction)
+            print(sentiment)
 
-        print(len(results))
+            if sentiment == 1:
+                q += " AND (sentiment:1 OR sentiment:0)"
+            if sentiment == -1:
+                q += " AND (sentiment:-1 OR sentiment:0)"
+            print(q)
+    parser = MultifieldParser(["title", "content"], ix.schema)
+    # parser = QueryParser("content", ix.schema)
+    parser.add_plugin(DateParserPlugin())
+    q = parser.parse(q)
 
-        display_text = f"Title: {title}\nContent: {content}\nURL: {url}\n\n"
-        self.results_text.insert(tk.END, display_text)
+    # results = searcher.search(query, limit=20) || results = searcher.search(query, limit=None)
+    res = searcher.search(q, limit=None)
+
+    # Formatta i risultati direttamente nella funzione
+    formatted_results = []
+    for result in res:
+        title = result.get('title')
+        date = result.get('date')
+        url = result.get('url')
+        content = result.get('content')
+
+        if content is not None:
+            #Snippet Articolo
+            content = content[:100] + '...'
+            total +=1
+        else:
+            print("Snippet dell'Articolo non disponibile")
+
+        formatted_result = f"Titolo: {title}\n{date}\n{content}\nURL: {url}\n\n"
+        formatted_results.append(formatted_result)
+
+    print(f"Totale: {total}")
+
+    return formatted_results
 
 
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    app = SearchArticle(root)
-    root.mainloop()
+    classifier = pipeline("text-classification", model='nlptown/bert-base-multilingual-uncased-sentiment', top_k=2)
+    V2 = True
+    V3 = False
+    query = input("Inserisci ciò che vuoi cercare (default: contenuto) \nSuggerito: 'Allarmante!': ")
+    results = execute_query(query, classifier, V2, V3, False, False)
+    for result in results:
+        print(result)
+

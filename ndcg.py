826d7c8f0bb59dbbf0fa4271e5201323047b9473
Yuukira02 <123182811+Yuukira02@users.noreply.github.com>
2024-01-30 21:55:32 +0100

    Add files via upload

---
+++ ndcg.py
@@ -0,0 +1,109 @@
+from transformers import pipeline
+
+from searcher import execute_query
+from sklearn.metrics import dcg_score
+import numpy as np
+
+# VARIABILI GLOBALI NECESSARIE. TO-DO: Ad un utilizzo più ampio, renderemo la lettura del file benchmark.txt più scalato
+# VANE: ho utilizzato un benchmark.txt che aveva valori NUM_QUERY = 2 e NUM_R_DOCS = 20
+# FATI: avrà un benchmark iniziale con 7 query (base) e 10 risultati per query.
+NUM_QUERY = 15
+NUM_R_DOCS = 20
+V2 = True
+V3 = False
+
+
+# ALGORITMO CHE LEGGE LA CLASSE BENCHMARK[15 query[20 url]]
+# nota: la benchmark è una LISTA di DIZIONARI (query) che associano url (chiavi) a un valore di rilevanza arbitrario.
+def read_benchmark():
+    path = "benchmark.txt"
+    user_query = []
+    benchmark = []
+    try:
+        fileobj = open(path, 'r')
+    except Exception as e:
+        print(e)
+        exit()
+
+    for e in range(NUM_QUERY):
+        query = fileobj.readline().removesuffix('\n')
+        ideal_answers = {}
+        for i in range(NUM_R_DOCS):
+            relevance = int(fileobj.read(1))
+            # per scartare spazio metasimbolico e non rilevante:
+            fileobj.read(1)
+            url = fileobj.readline().rstrip('\n')
+            ideal_answers[url] = relevance
+        benchmark.append(ideal_answers)
+        # per scartare righe con significato metasimbolico e non rilevante
+        fileobj.readline()
+        user_query.append(query)
+
+    fileobj.close()
+    return user_query, benchmark
+
+
+# ALGORITMO CHE DATE LE RISPOSTE urls[10] ad UNA query, compara le risposte con il benchmark ed assegna un valore
+def init_scores(eng_results, bench_results):
+    scores = {}
+    urls = []
+    for i in range(eng_results.scored_length()):
+        # equivale a eng_results[i].get('url'), ossia accedere ad una lista di dizionari.
+        urls.append(eng_results[i]['url'])
+
+    for url in urls:
+        if str(url) in bench_results:
+            scores[url] = bench_results.get(url)
+        else:
+            scores[url] = 0
+    return scores
+
+
+def main():
+    classifier = pipeline("text-classification", model='nlptown/bert-base-multilingual-uncased-sentiment', top_k=2)
+    query, benchmark = read_benchmark()
+    media = 0
+
+    if V3 and V2:
+        print("Stiamo utilizzando la V3:")
+    elif V2:
+        print("Stiamo utilizzando la V2:")
+    elif not V3 and not V3:
+        print("Stiamo utilizzando la V1:")
+    else:
+        print("Utilizzando una versione ibrida: ")
+
+    # per ogni query in benchmark (tot= NUM_QUERY) , esegue la ndcg.
+    for i in range(len(benchmark)):
+
+        # estrai i risultati per la query i di 7 (=NUM_QUERY)
+        results = execute_query(query[i], classifier, correct_spelling=V2, synonym=V2, sentiment_analysis=V3)
+        rel_scores = init_scores(results, benchmark[i])
+
+        # estraggo i valori (url, val) dalla query i-esima dal benchmark
+        gold_standard = benchmark[i]
+
+        # controllo: se i documenti recuperati non sono abbastanza,
+        # se non sono abbastanza, paddo i rimanenti valori con 0
+        rel_scores = list(rel_scores.values())
+        rel_scores = (rel_scores + NUM_R_DOCS * [0])[:NUM_R_DOCS]
+
+        # poi ne estraggo semplicemente la lista di valori ideali per confrontarlo con i valori reali
+        # NOTA: effettuiamo conversione con numpy
+        gold_standard = np.asarray([list(gold_standard.values())])
+        rel_scores = np.asarray([rel_scores])
+
+        dcg = dcg_score(rel_scores, gold_standard)
+        idcg = dcg_score(gold_standard, gold_standard)
+        ndcg = dcg / idcg
+        print(f"Query numero {i+1}: {ndcg}")
+        media += ndcg
+
+    # Infine calcoliamo la media di tutti i ndcg per l'intero set di query.
+    # Così otteniamo un calcolo dell'efficienza dell'IR.
+    media = media / len(benchmark)
+    print(f"\nNDCG dell'intero IR: {media}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ searcher.py
@@ -0,0 +1,148 @@
+import re
+import nltk
+from whoosh.index import open_dir, exists_in
+from autocorrect import Speller
+from whoosh.qparser import MultifieldParser
+from whoosh.qparser.dateparse import DateParserPlugin
+from nltk.corpus import wordnet as wn
+from transformers import pipeline
+from indexing import convert_predicted_sentiment, create_index
+
+
+def format_results(res):
+    formatted_results = []
+    for r in res:
+        title = r.get('title')
+        date = r.get('date')
+        url = r.get('url')
+        content = r.get('content')
+
+        if content is not None:
+            # Snippet Articolo
+            content = content[:100] + '...'
+        else:
+            print("Snippet dell'Articolo non disponibile")
+
+        formatted_result = f"Titolo: {title}\n{date}\n{content}\nURL: {url}\n\n"
+        formatted_results.append(formatted_result)
+    return formatted_results
+
+
+def disambiguate_terms(terms):
+    # controllo iniziale:
+    try:
+        # Prova a cercare il corpus di WordNet
+        nltk.data.find('corpora/wordnet')
+    except LookupError:
+        # Se non trova il corpus, scaricalo
+        nltk.download('wordnet')
+
+    related_words = set()
+    for t_i in terms:  # t_i is the target term
+        sel_sense = None
+        sel_score = 0.0
+
+        for s_ti in wn.synsets(t_i, lang='ita', pos=wn.NOUN):
+            score_i = 0.0
+            for t_j in terms:  # t_j term in t_i's context window
+                if t_i == t_j:
+                    continue
+                best_score = 0.0
+                for s_tj in wn.synsets(t_j, lang='ita', pos=wn.NOUN):
+                    temp_score = s_ti.wup_similarity(s_tj)
+                    if temp_score > best_score:
+                        best_score = temp_score
+                score_i = score_i + best_score
+            if score_i > sel_score:
+                sel_score = score_i
+                sel_sense = s_ti
+        if sel_sense is not None:
+            for sense in sel_sense.lemmas('ita'):
+                related_words.add(sense.name())
+    return related_words
+
+
+def get_related_words(word):
+    related_words = set()
+
+    tokens = re.split(',|_|-|!| OR | NOT | AND |title:|content:|date:|sentiment:| ', word)
+    if len(tokens) > 1:
+        # Estendi con synset disambiguati dal contesto
+        related_words = disambiguate_terms(tokens)
+    else:
+        # Aggiungi sinonimi
+        for syn in wn.synsets(tokens[0], pos=wn.NOUN, lang='ita'):
+            for lemma in syn.lemmas('ita'):
+                related_words.add(lemma.name())
+
+    return list(related_words)
+
+
+def correction(word):
+    # Bypassa la correzione ortografica per la parola chiave "NOT"
+    if (word.upper() == 'NOT') or (word.upper() == '(NOT'):
+        return word
+    else:
+        # Correttore ortografico
+        spell = Speller(lang='it')
+        return spell(word)
+
+
+def execute_query(original_query, classifier, sentiment_analysis, correct_spelling, synonym):
+    """ALGORITMO CHE DATA UNA QUERY ESEGUE LE RISPOSTE"""
+
+    if not exists_in("indexdir"):
+        print("Sto usando indexer da searcher")
+        create_index()
+
+    ix = open_dir("indexdir")
+    searcher = ix.searcher()
+    q = original_query
+
+    if correct_spelling:
+        # Applica la correzione ortografica solo per le parole non booleane
+        corrected_query = " ".join(correction(word) for word in q.split())
+        q = corrected_query
+
+    if synonym:
+        # Ottieni sinonimi della parola inserita
+        synonyms = get_related_words(q)
+
+        # Aggiugni la parola originale e i sinonimi alla query
+        if len(synonyms) != 0:
+            q = q + " AND (" + f"{' OR '.join(synonyms)}" + ")"
+
+    # se stiamo eseguendo la V3 dell'engine, esegui
+    if sentiment_analysis:
+        if "sentiment" not in original_query:
+            # valuta la sentiment di una query per aumentare la capacità espressiva
+            prediction = classifier(original_query)
+            sentiment = convert_predicted_sentiment(prediction)
+
+            if sentiment == 1:
+                q += " AND (sentiment:1 OR sentiment:0)"
+            if sentiment == -1:
+                q += " AND (sentiment:-1 OR sentiment:0)"
+
+    parser = MultifieldParser(["title", "content"], ix.schema)
+    parser.add_plugin(DateParserPlugin())
+    q = parser.parse(q)
+
+    ''''RIGA DI CODICE DA CAMBIARE MANUALMENTE DIPENDENTEMENTE SE SI UTILIZZA LA NDCG O LA GUI:'''
+    # res = searcher.search(q, limit=20)
+    res = searcher.search(q, limit=None)
+
+    # Formatta i risultati direttamente nella funzione
+    # res = format_results(res)
+
+    return res
+
+
+if __name__ == "__main__":
+    sentiment_classifier = pipeline("text-classification",
+                                    model='nlptown/bert-base-multilingual-uncased-sentiment', top_k=2)
+    V3 = False
+    query = input("Inserisci ciò che vuoi cercare (default: contenuto) \nSuggerito: 'Allarmante!': ")
+    results = execute_query(query, sentiment_classifier, V3, True, True)
+    for result in results:
+        print(result['url'])
